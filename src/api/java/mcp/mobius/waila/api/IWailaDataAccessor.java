37d3310fd0f63d1fe5282dfc379762ed94478ae7
Rukachan <rukachan@users.noreply.github.com>
2015-11-07 20:43:23 +0200

    updated apis, updated the plugin system

--- src/api/java/mcp/mobius/waila/api/IWailaDataAccessor.java
+++ src/api/java/mcp/mobius/waila/api/IWailaDataAccessor.java
@@ -10,17 +10,21 @@ import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
-/* The Accessor is used to get some basic data out of the game without having to request
- * direct access to the game engine.
- * It will also return things that are unmodified by the overriding systems (like getWailaStack).
+/**
+ * The Accessor is used to get some basic data out of the game without having to request direct access to the game engine.<br>
+ * It will also return things that are unmodified by the overriding systems (like getWailaStack).<br>
+ * An instance of this interface is passed to most of Waila Block/TileEntity callbacks.
+ * @author ProfMobius
+ *
  */
 
-public interface IWailaDataAccessor {
+public interface IWailaDataAccessor{
 		
 	World        		 getWorld();
 	EntityPlayer 		 getPlayer();
 	Block        		 getBlock();
 	int          		 getBlockID();
+	String               getBlockQualifiedName();
 	int          		 getMetadata();
 	TileEntity           getTileEntity();
 	MovingObjectPosition getPosition();
